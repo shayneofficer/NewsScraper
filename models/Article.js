e760bdf040ab62779b68f3d4a5a82f3da86b48af
shayneofficer <shayneofficer@gmail.com>
2019-01-14 21:08:34 -0600

    models and some server stuff

---
+++ models/Article.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+    headline: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    summary: {
+        type: String,
+        trim: true,
+    },
+    url: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    photo: {
+        type: String,
+        trim: true
+    },
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
--- server.js
+++ server.js
@@ -3,44 +3,64 @@ var handlebars = require("express-handlebars");
 var mongoose = require("mongoose");
 var logger = require("morgan");
 
-
+// Scraping tools
 var axios = require("axios");
 var cheerio = require("cheerio");
 
+// Require all models
+var db = require('./models');
+
 var PORT = 3000;
 
+// Initialize Express
 var app = express();
 
+// Configure middleware
+// Use morgan logger for logging requests
 app.use(logger('dev'));
-
+// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+// Make public a static folder
 app.use(express.static('public'));
 
-var databaseUrl = "mongodb://localhost/test_db"
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
 
-if (process.env.MONGODB_URI) {
-    mongoose.connect(process.env.MONGODB_URI);
-} else {
-    mongoose.connect(databaseUrl, { useNewUrlParser: true });
-}
+// Connect to the MongoDB
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-var db = mongoose.connection;
-db.on('error', function (err) {
-    console.log('Mongoose Error: ', err);
-});
-db.once('open', function () {
-    console.log('Mongoose connection successful.');
-})
+// Routes
 
+// Default home GET route
 app.get("/", function (req, res) {
-    db.Test.create({
-        "working": "yes"
+
+    db.Article.find({}).then(function (dbArticle) {
+        res.json(dbArticle);
+    }).catch(function (err) {
+        res.json(err);
     });
-    res.sendFile("index.html");
+
+    // res.sendFile("index.html");
 })
 
+app.get("/scrape", function (req, res) {
+
+    axios.get("https://www.chicagobusiness.com/").then(function (response) {
+        var $ = cheerio.load(response.data);
+
+        $("div.middle-article-block").each(function (i, element) {
+            var result = {};
+
+            result.title = $(element).find("div.middle-article-headline").text();
+
+            console.log(result);
+        });
+    });
+    res.send("Scrape Complete");
+});
+
+// Start the server
 app.listen(PORT, function () {
-    console.log("App listening on port " + PORT + "!");
+    console.log("App listening on port http://localhost:" + PORT + " !");
 })
